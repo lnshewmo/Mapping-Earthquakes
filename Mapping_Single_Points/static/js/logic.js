5be95957a0b1cf4bab568ea1358ecbe256299563
Laura Miller <laura.shewmon@gmail.com>
2022-08-17 17:51:23 -0400

    polyline added

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,18 +2,33 @@
 console.log('working');
 
 // create the map object with a center and zoom level
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([34, -100], 4);
+
+let line = [
+    [37.6213, -122.3790],
+    [30.18999924, -97.668663992],
+    [43.6232, -79.3910],
+    [40.641766, -73.780968]
+];
+
+L.polyline(line, {
+    color: "blue",
+    dashArray: 5,
+    opacity: 0.5
+
+
+}).addTo(map);
 
 //  Add a marker to the map for Los Angeles, California.
-L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    color: "black",
-    fillColor: "#ffffa1"
-}).addTo(map);
+// L.circleMarker([34.0522, -118.2437], {
+//     radius: 25,
+//     color: "black",
+//     fillColor: "#ffffa1"
+// }).addTo(map);
 
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
